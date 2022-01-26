3e77a4ced05657850327532ed1ce34f520b50228
Ling Balmforth <Ling.Balmforth@softwire.com>
2022-01-26 11:58:08 +0000

    bishop passes test!

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -12,32 +12,27 @@ export default class Bishop extends Piece {
         let move;
 
         // all possible moves in the up positive diagonal 
-  
-        console.log(`result is ${res}`);
 
-        for (let i = 1; i >= Math.min(location.row, location.col); i++) {
-            move = Square.at(1, 2);
-           // console.log(`move is ${move}`);
-            console.log(`Min move is ${Math.min(location.row, location.col)}`);
+        for (let i = 0; i < Math.min(7-location.row, 7-location.col); i++) {
+            move = Square.at(location.row + (i+1), location.col + (i+1));
             movesArray.push(move);
         }
         // all possible moves in the up negative diagonal
-        for (let i = 1; i >= Math.min(location.row, location.col); i++) {
-            move = Square.at(location.row + 1, location.col - 1);
+        for (let i = 0; i < Math.min(7-location.row, location.col); i++) {
+            move = Square.at(location.row + (i+1), location.col - (i+1));
             movesArray.push(move);
         }
         //all possible moves in the down positive diagonal
-        for (let i = 1; i >= Math.min(location.row, location.col); i++) {
-            move = Square.at(location.row - 1, location.col + 1);
+        for (let i = 0; i < Math.min(location.row, 7-location.col); i++) {
+            move = Square.at(location.row - (i+1), location.col + (i+1));
             movesArray.push(move);
         }
         // all possible moves in the down negative diagonal
-        for (let i = 1; i >= Math.min(location.row, location.col); i++) {
-            move = Square.at(location.row - 1, location.col - 1);
+        for (let i = 0; i < Math.min(location.row, location.col); i++) {
+            move = Square.at(location.row - (i+1), location.col - (i+1));
             movesArray.push(move);
         }
 
-         console.log(`movesArray is ${movesArray}`);
         return movesArray;
 
     }}
